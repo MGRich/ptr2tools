953e419a7cb73d88ceb5c4575a8f4493d9ddedbb
RMGRich <rmgrichdeveloper@gmail.com>
2020-03-08 04:35:22 -0500

    other sht

--- ptr2app/ptr2app.cpp
+++ ptr2app/ptr2app.cpp
@@ -5,18 +5,65 @@
 #include <Windows.h>
 
 #define ALIGN(x, y) (((x) + (y-1)) & (~((y)-1)))
-
 int main(int argc, char* argv[])
 {
-    typedef int (*ext)(char*, char*);
-    typedef int (*lst)(char*);
+    typedef int (*two)(char*, char*);
+    typedef int (*one)(char*);
+    auto ptr2lib = LoadLibraryA("ptr2lib.dll");
+    auto analyze = (one)GetProcAddress(ptr2lib, "spmanalyze");
+    analyze(argv[1]);
+}//*/
+
+//PTR2INT
+/*int main(int argc, char* argv[]) {
+    typedef int (*two)(char*, char*);
+    typedef int (*one)(char*);
     auto ptr2int = LoadLibraryA("ptr2lib.dll");
-    auto extract = (ext)GetProcAddress(ptr2int, "intextract");
-    auto list = (lst)GetProcAddress(ptr2int, "intlist");
-    auto comp = (ext)GetProcAddress(ptr2int, "intcreate");
-    list(argv[1]);
-    extract(argv[1], argv[2]);
-    comp(argv[1], argv[2]);
+    if (!ptr2int) {
+        printf("The ptr2lib.dll library couldn't be found! Please make sure it exists.");
+        return 1;
+    }
+
+    auto inArgs = [argc, argv](std::string arg) {
+
+        for (int i = 1; i < argc; i++) {
+            if (!strcmp(argv[i], arg.c_str())) {
+                return true;
+            }
+        }
+        return false;
+    };
+    auto argDetail = [argc, argv, inArgs](std::string arg) {
+        if (!inArgs(arg)) return std::string();
+        for (int i = 1; i < argc; i++) {
+            if (!strcmp(argv[i], arg.c_str())) {
+                return std::string(argv[i + 1]);
+            }
+        }
+        return std::string();
+    };
+    
+
+    if (inArgs("e") || inArgs("extract")) {
+        auto extract = (two)GetProcAddress(ptr2int, "intextract");
+        return extract(argv[2], argv[3]);
+    }
+    if (inArgs("l") || inArgs("list")) {
+        auto list = (one)GetProcAddress(ptr2int, "intlist");
+        return list(argv[2]);
+    }
+    if (inArgs("c") || inArgs("create") || inArgs("compress")) {
+        auto comp = (two)GetProcAddress(ptr2int, "intcreate");
+        return comp(argv[2], argv[3]);
+    }
+
+    printf("PTR2INT [RMG REWRITE]\n-------------\n");
+    printf("extract [intfile] [outfolder]\n");
+    printf("  Extracts intfile to outfolder.\n");
+    printf("list [intfile]\n");
+    printf("  Lists all files in the INT file specified.\n");
+    printf("create [intfile] [infolder]\n");
+    printf("  Packs infolder into an INT. posesix ptr2int folders work too.\n");
 }//*/
 
 /*int main() {
